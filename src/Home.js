781dd782b4e1d006c6676810aa336da33535c08f
Rashitha Alahakoon <rashithaalahakoon@gmail.com>
2024-06-10 13:10:45 +0530

    using map method

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,23 @@ import {useState} from 'react';
 
 const Home = () => {
 
-    // let name = 'mario';
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () =>{
-        setName('liugi');
-        setAge(30);
-    }
 
     return (
         <div className="home">
-            <h2>Home Page</h2>
-            <button onClick={handleClick}>Click me</button>
-            <p>{ name } is {age} years old.</p>
+            {
+                blogs.map((blog) => (
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                    </div>
+                ))
+            }
         </div>
       );
 }
